d428c8ee38472e8f7e762eb378055665b00ca5d1
Erick Gabriel <erickgabrielalves@hotmail.com>
2025-03-29 18:11:02 -0300

    feat: generate random e

--- keys.py
+++ keys.py
@@ -1,16 +1,20 @@
 from prime import gerar_primo
 from math import gcd
+import random
 
 def gerar_chaves(bits=128):
     p = gerar_primo(bits // 2)
     q = gerar_primo(bits // 2)
     n = p * q
     phi = (p - 1) * (q - 1)
-    e = 65537
+
+    e = random.randrange(3, phi, 2)
     while gcd(e, phi) != 1:
-        e += 2
+        e = random.randrange(3, phi, 2)
+    
     d = pow(e, -1, phi)
     return (n, e), (n, d)
+
 
 def salvar_chave(nome_arquivo, chave):
     with open(nome_arquivo, "w") as f:
